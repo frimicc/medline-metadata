b3adce516f12ee7ad035dc93f13453f712199442
Michael Friedman <frimicc@gmail.com>
2012-10-22 20:44:23 -0700

    Added subdirs for large amounts of data

--- lib/Medline/Metadata.pm
+++ lib/Medline/Metadata.pm
@@ -20,25 +20,48 @@ has 'dir' => (
     isa => 'Str',
 );
 
+has 'use_subdirs' => (
+    is      => 'rw',
+    isa     => 'Bool',
+    default => 0,
+);
+
 sub split_and_create_citation_files {
     my $self = shift;
 
     die $self->filepath() . ' does not exist!' unless -e $self->filepath();
-    unless ( -e $self->dir() ) {
-        make_path( $self->dir() );
-    }
 
-    my $doc = XML::LibXML->load_xml( location => $self->filepath() );
-    my $root = $doc->documentElement();
+    my $doc       = XML::LibXML->load_xml( location => $self->filepath() );
+    my $root      = $doc->documentElement();
     my @citations = $root->getChildrenByTagName('MedlineCitation');
     foreach my $citation (@citations) {
-        my $pmid = $citation->findvalue('PMID' );
-        my $path = $self->dir() . '/' . $pmid . '.xml';
+        my $pmid = $citation->findvalue('PMID');
+
+        my $dir = $self->medline_path($pmid);
+        make_path( $dir ) unless ( -e $dir );
+        my $path = $dir . '/' . $pmid . '.xml';
+
         open my $fh, '>:encoding(UTF-8)', $path or die "Can't write to $path";
         print $fh $citation->toString();
         close $fh;
     }
     return;
+}
+
+sub medline_path {
+    my $self = shift;
+    my $pmid = shift;
+    my ($dir);
+
+    if ( $self->use_subdirs() ) {
+        my $subdir = substr( $pmid, -3 );
+        $dir = $self->dir() . '/' . $subdir;
+    }
+    else {
+        $dir = $self->dir();
+    }
+
+    return $dir;
 }
 
 sub delete_citation_files {
--- t/Metadata.t
+++ t/Metadata.t
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 9;
+use Test::More tests => 15;
 use Test::Exception;
 
 use_ok('Medline::Metadata');
@@ -14,6 +14,7 @@ ok( $meta = Medline::Metadata->new( filepath => 't/sample_metadata.xml' ),
     'Loaded sample file' );
 isa_ok( $meta, 'Medline::Metadata' );
 
+# Test without subdirs
 ok( $meta->dir('t/citations'), 'added citations dir' );
 is( $meta->split_and_create_citation_files(), undef,
     'split_and_create_citation_files'
@@ -21,4 +22,15 @@ is( $meta->split_and_create_citation_files(), undef,
 ok( -e $meta->dir(), 'split_and_create_citation_files created dir' );
 is( $meta->delete_citation_files(), undef, 'delete_citation_files' );
 is( -e $meta->dir(), undef, 'delete_citation_files removed dir' );
+
+# Test with subdirs
+ok($meta->use_subdirs(1), 'use_subdirs');
+is( $meta->split_and_create_citation_files(), undef,
+    'split_and_create_citation_files with subdirs'
+);
+ok( -e $meta->dir(), 'split_and_create_citation_files created dir' );
+ok( -e $meta->dir(). '/395', 'split_and_create_citation_files created subdir' );
+is( $meta->delete_citation_files(), undef, 'delete_citation_files' );
+is( -e $meta->dir(), undef, 'delete_citation_files removed dir' );
+
 
